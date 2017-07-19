34f4dc0677286679cc6bb955ab3b833c64568c08
Nazmul Hasan Rupok <re.enter.rupok@gmail.com>
2017-07-19 22:19:40 +0600

    gulp task updated

--- framework/assets/gulpfile.js
+++ framework/assets/gulpfile.js
@@ -3,6 +3,20 @@ var gulp = require('gulp'),
  	cleanCSS = require('gulp-clean-css');
 
 
+/**
+ * Files and directories we reference in our tasks below. The "toCompile" properties
+ * also generally contain patterns of files that are watched to trigger compilation.
+ */
+var files = {
+	sass: {
+		toCompile: ['sass/*.scss', 'sass/*/*.scss']
+	}
+};
+
+// -------------
+// Define Tasks.
+// -------------
+
 gulp.task('default', function() {
   // do nothing
 });
@@ -11,3 +25,16 @@ gulp.task('minify-css', () => {
   return gulp.src('site/css/styles/*.css')
     .pipe(gulp.dest('dist'));
 });
+
+gulp.task('compass', function() {
+  return gulp.src(files.sass.toCompile)
+    .pipe(compass({
+      config_file: 'config.rb',
+      css: 'site/css/',
+      sass: 'sass/'
+    }))
+    .pipe(gulp.dest('./' + compassConfig.css))
+});
+
+
+
--- framework/functions/enqueue/styles.php
+++ framework/functions/enqueue/styles.php
@@ -10,8 +10,7 @@
  */
 
 function flexia_site_styles() {
-	wp_enqueue_style( 'flexia-style', get_stylesheet_uri() );
-	wp_enqueue_style( 'flexia-theme-style', get_template_directory_uri() . '/framework/assets/site/css/styles.css');
+	wp_enqueue_style( 'flexia-theme-style', get_template_directory_uri() . '/framework/assets/site/css/style.css');
 
 }
 add_action( 'wp_enqueue_scripts', 'flexia_site_styles' );
