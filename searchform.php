953b3db3e105e0459c47e68d25c87314aaab4de0
Nazmul Hasan Rupok <re.enter.rupok@gmail.com>
2017-07-21 12:04:20 +0600

    meta style updated

---
+++ searchform.php
@@ -0,0 +1,5 @@
+<form action="<?php echo site_url();?>" class="search-form" method="get" role="search">
+	<span class="screen-reader-text">Search for:</span>
+	<input type="search"  title="Search for:" name="s" value="" placeholder="Search …" class="form-control search-field">
+	<button type="submit" class="search-submit"><i class="fa fa-search"></i></button>
+</form>
