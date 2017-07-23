98abbead9d64649a836d73a49d50229ebe5f3848
Nazmul Hasan Rupok <re.enter.rupok@gmail.com>
2017-07-23 15:29:46 +0600

    404 page added

--- 404.php
+++ 404.php
@@ -9,52 +9,35 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
 
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'flexia' ); ?></h1>
-				</header><!-- .page-header -->
+<div id="page" class="site">
 
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'flexia' ); ?></p>
+	<?php get_template_part( 'framework/views/template-parts/content', 'masthead' ); ?>
+	
+	<header class="page-header error-header">
+        <div class="header-inner">
+            <div class="header-content">
+                <h2 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'flexia' ); ?></h2>
+            </div>
+        </div>
+    </header>
 
-					<?php
-						get_search_form();
+	<div id="content" class="site-content">
+		<div class="flexia-wrapper flexia-container">
+			<div id="primary" class="content-area">
+				<main id="main" class="site-main flexia-container">
+					<section class="error-404 not-found">
+						<p><?php esc_html_e( 'Bummer! The page you are looking for is no longer here, or never existed in the first place. You can try searching for what you are looking for using the form below. If that still doesn\'t provide the results you are looking for, you can always start over from the home page.', 'flexia' ); ?></p>
 
-						the_widget( 'WP_Widget_Recent_Posts' );
-					?>
+						<?php get_search_form(); ?>
+					</section>
+				</main><!-- #main -->
+			</div><!-- #primary -->
 
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'flexia' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'flexia' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-
-						the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+		</div><!-- #flexia-wrapper -->
+	</div><!-- #content --> 
+</div><!-- #page -->
 
 <?php
+get_template_part( 'framework/views/template-parts/content', 'footer' );
 get_footer();
