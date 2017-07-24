e823051c59ac0db1add9fbf7497f3c2316598da5
Nazmul Hasan Rupok <re.enter.rupok@gmail.com>
2017-07-24 23:19:38 +0600

    navigation menu style added

--- framework/views/template-parts/content-navbar.php
+++ framework/views/template-parts/content-navbar.php
@@ -21,12 +21,6 @@
 			<?php else : ?>
 				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
 			<?php
-			endif;
-
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
 			endif; ?>
 		</div><!-- .site-branding -->
 
