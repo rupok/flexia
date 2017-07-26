df6cfef2ea2ae4d92c5fc761f977e2faa60ddbbc
Nazmul Hasan Rupok <re.enter.rupok@gmail.com>
2017-07-26 14:49:43 +0600

    blog post layout fixed

--- single.php
+++ single.php
@@ -13,7 +13,7 @@ get_header(); ?>
 <?php get_template_part( 'framework/views/template-parts/content', 'masthead' ); ?>
 
 	<div id="content" class="site-content">
-		<div class="flexia-wrapper flexia-container max width">
+		<div class="flexia-wrapper flexia-container">
 			<div id="primary" class="content-area">
 				<main id="main" class="site-main flexia-container">
 
