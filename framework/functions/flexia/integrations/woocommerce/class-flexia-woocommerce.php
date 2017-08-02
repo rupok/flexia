8144fb1fe9a03f0a08c0a403b7980524fbc701a3
Nazmul Hasan Rupok <re.enter.rupok@gmail.com>
2017-08-03 00:09:40 +0600

    product image flip added

--- framework/functions/flexia/integrations/woocommerce/class-flexia-woocommerce.php
+++ framework/functions/flexia/integrations/woocommerce/class-flexia-woocommerce.php
@@ -26,7 +26,9 @@ if ( ! class_exists( 'Flexia_WooCommerce' ) ) :
 			add_filter( 'woocommerce_product_thumbnails_columns', 	array( $this, 'thumbnail_columns' ) );
 			add_filter( 'loop_shop_columns', 						array( $this, 'shop_columns' ) );
 			add_filter( 'loop_shop_per_page', 						array( $this, 'products_per_page' ) );
-			add_filter( 'woocommerce_breadcrumb_defaults',          array( $this,'change_breadcrumb_delimiter' ) );
+			add_filter( 'woocommerce_breadcrumb_defaults',          array( $this, 'change_breadcrumb_delimiter' ) );
+			add_action( 'woocommerce_before_shop_loop_item_title',  array( $this, 'product_secondary_thumbnail' ), 11 );
+			add_filter( 'post_class', 								array( $this, 'product_has_gallery' ) );
 
 		}
 
@@ -82,6 +84,75 @@ if ( ! class_exists( 'Flexia_WooCommerce' ) ) :
 		public function change_breadcrumb_delimiter( $defaults ) {
 			$defaults['delimiter'] = '<span class="breadcrumb-separator"> <i class="fa fa-angle-right" aria-hidden="true"></i></span>';
 			return $defaults;
+		}
+
+		/**
+		 * Flip image on hover
+		 */
+
+		public function product_has_gallery( $classes ) {
+			global $product;
+
+			$post_type = get_post_type( get_the_ID() );
+
+			if ( ! is_admin() ) {
+
+				if ( $post_type == 'product' ) {
+
+					$attachment_ids = $this->get_gallery_image_ids( $product );
+
+					if ( $attachment_ids ) {
+						$classes[] = 'product-has-gallery';
+					}
+				}
+			}
+
+			return $classes;
+		}
+
+
+		/**
+		 * Frontend functions
+		 */
+		public function product_secondary_thumbnail() {
+			global $product, $woocommerce;
+
+			$attachment_ids = $this->get_gallery_image_ids( $product );
+
+			if ( $attachment_ids ) {
+				$attachment_ids     = array_values( $attachment_ids );
+				$secondary_image_id = $attachment_ids['0'];
+
+				$secondary_image_alt = get_post_meta( $secondary_image_id, '_wp_attachment_image_alt', true );
+				$secondary_image_title = get_the_title($secondary_image_id);
+
+				echo wp_get_attachment_image(
+					$secondary_image_id,
+					'shop_catalog',
+					'',
+					array(
+						'class' => 'secondary-image attachment-shop-catalog wp-post-image wp-post-image--secondary',
+						'alt' => $secondary_image_alt,
+						'title' => $secondary_image_title
+					)
+				);
+			}
+		}
+
+
+		/**
+		 * WooCommerce Compatibility Functions
+		 */
+		public function get_gallery_image_ids( $product ) {
+			if ( ! is_a( $product, 'WC_Product' ) ) {
+				return;
+			}
+
+			if ( is_callable( 'WC_Product::get_gallery_image_ids' ) ) {
+				return $product->get_gallery_image_ids();
+			} else {
+				return $product->get_gallery_attachment_ids();
+			}
 		}
 
 
