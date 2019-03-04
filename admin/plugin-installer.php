f7a60f6d2b5485f83d1b2f90bee167f66584b933
Sabuz <naz.sabuz@gmail.com>
2019-03-04 17:33:24 +0600

    Added: Admin panel inside theme. Removed: TGM

---
+++ admin/plugin-installer.php
@@ -0,0 +1,322 @@
+<?php
+
+if (!class_exists('Flexia_Plugin_Installer')) {
+
+    /**
+     * Flexia Plugin Installer Class
+     *
+     * @since     1.0.0
+     */
+    class Flexia_Plugin_Installer
+    {
+        public $free_plugins;
+        public $pro_plugins;
+
+        public function __construct()
+        {
+            add_action('admin_footer', array($this, 'free_plugins_activation_script'));
+            add_action('wp_ajax_flexia_free_plugin_installer', array($this, 'free_plugins_installer'));
+        }
+
+        /**
+         * Fail if plugin install/activation fails
+         *
+         * @since   1.0.0
+         */
+        public function fail_on_error($thing)
+        {
+            if (is_wp_error($thing)) {
+                wp_send_json_error($thing->get_error_message());
+            }
+        }
+
+        /**
+         * Generates all free plugins
+         *
+         * @since   1.0.0
+         */
+        public function free_plugins()
+        {
+            include_once ABSPATH . 'wp-admin/includes/plugin-install.php';
+            include_once ABSPATH . 'wp-admin/includes/class-wp-upgrader.php';
+
+            $this->free_plugins = array(
+                array(
+                    'slug' => 'flexia-core',
+                    'file' => 'flexia-core.php',
+                ),
+                array(
+                    'slug' => 'elementor',
+                    'file' => 'elementor.php',
+                ),
+                array(
+                    'slug' => 'essential-addons-for-elementor-lite',
+                    'file' => 'essential-addons-for-elementor.php',
+                ),
+                array(
+                    'slug' => 'woocommerce',
+                    'file' => 'woocommerce.php',
+                ),
+                array(
+                    'slug' => 'wordpress-seo',
+                    'file' => 'wp-seo-main.php',
+                ),
+            );
+
+            foreach ($this->free_plugins as $plugin) {
+                $slug_name = $plugin['slug'];
+                $file_name = $plugin['file'];
+                $plugin_basename = $plugin['slug'] . '/' . $plugin['file'];
+                $api = plugins_api('plugin_information',
+                    array(
+                        'slug' => $plugin['slug'],
+                        'fields' => array(
+                            'short_description' => true,
+                            'sections' => false,
+                            'requires' => false,
+                            'downloaded' => true,
+                            'last_updated' => false,
+                            'added' => false,
+                            'tags' => false,
+                            'compatibility' => false,
+                            'homepage' => false,
+                            'donate_link' => false,
+                            'icons' => false,
+                            'banners' => true,
+                        ),
+                    )
+                );
+                $main_plugin_file = $this->get_plugin_file($plugin['slug']);
+
+                if (!empty($main_plugin_file) && !is_plugin_active($main_plugin_file)) {
+                    $btn_class = 'button button-primary';
+                    $btn_text = __('Activate', 'flexia');
+                } else if (is_plugin_active($main_plugin_file)) {
+                    $btn_class = 'button button-secondary disabled';
+                    $btn_text = __('Activated', 'flexia');
+                } else {
+                    $btn_class = 'activate button button-secondary';
+                    $btn_text = __('Install Now', 'flexia');
+                }
+
+                $this->render_free_template($plugin, $api, $slug_name, $file_name, $btn_class, $btn_text);
+            }
+        }
+
+        /**
+         * A method to get the main plugin file
+         *
+         * @since   1.0.0
+         */
+        public function get_plugin_file($plugin_slug)
+        {
+            require_once ABSPATH . '/wp-admin/includes/plugin.php';
+
+            $plugins = get_plugins();
+
+            foreach ($plugins as $plugin_file => $plugin_info) {
+                if ($slug = dirname(plugin_basename($plugin_file))) {
+                    if ($slug == $plugin_slug) {
+                        return $plugin_file;
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        /**
+         * Plugin Activation Script
+         *
+         * @since   1.0.0
+         */
+        public function free_plugins_activation_script()
+        {
+            echo '<script type="text/javascript">
+				jQuery(document).ready( function($) {
+					$(".install-btn").on("click", function(e) {
+						var self = $(this);
+						e.preventDefault();
+						var fileName = self.attr("data-file-name");
+						var slugName = self.attr("data-slug");
+						self.addClass("install-now updating-message");
+						self.text("' . esc_js("Installing...") . '");
+
+						$.ajax({
+							url: "' . admin_url("admin-ajax.php") . '",
+							type: "post",
+							data: {
+								action: "flexia_free_plugin_installer",
+								slug: slugName,
+								file: fileName,
+								_wpnonce: "' . wp_create_nonce("flexia_free_plugin_installer") . '"
+							},
+							success: function(response) {
+								self.text("' . esc_js("Activated") . '");
+								self.removeClass("button-primary");
+								self.addClass("button-secondary");
+								self.attr("disabled", "disabled");
+							},
+							error: function(error) {
+								self.text("' . esc_js("Try Again!") . '");
+								self.removeClass("install-now updating-message");
+							},
+							complete: function() {
+								self.removeClass("install-now updating-message");
+							}
+						});
+					});
+				} );
+			</script>';
+        }
+
+        /**
+         * Install \ Activate a plugin
+         *
+         * @since   1.0.0
+         */
+        public function free_plugins_installer()
+        {
+            check_ajax_referer('flexia_free_plugin_installer');
+
+            if (!current_user_can('manage_options')) {
+                wp_send_json_error(__('You don\'t have permission to install the plugins'));
+            }
+
+            $slug_name = $_POST['slug'];
+            $file_name = $_POST['file'];
+
+            $install_status = $this->install_a_free_plugin($slug_name, $file_name);
+            $this->fail_on_error($install_status);
+
+            wp_send_json_success();
+        }
+
+        /**
+         * Install a free plugin
+         *
+         * @since   1.0.0
+         */
+        public function install_a_free_plugin($slug, $file)
+        {
+            include_once ABSPATH . 'wp-admin/includes/plugin-install.php';
+            include_once ABSPATH . 'wp-admin/includes/class-wp-upgrader.php';
+
+            $plugin_basename = $slug . '/' . $file;
+
+            // if exists and not activated
+            if (file_exists(WP_PLUGIN_DIR . '/' . $plugin_basename)) {
+                return activate_plugin($plugin_basename);
+            }
+
+            // seems like the plugin doesn't exists. Download and activate it
+            $upgrader = new Plugin_Upgrader(new WP_Ajax_Upgrader_Skin());
+
+            $api = plugins_api('plugin_information', array('slug' => $slug, 'fields' => array('sections' => false)));
+            $result = $upgrader->install($api->download_link);
+
+            if (is_wp_error($result)) {
+                return $result;
+            }
+
+            return activate_plugin($plugin_basename);
+        }
+
+        /**
+         * Generates all premium plugins
+         *
+         * @since   1.0.0
+         */
+        public function premium_plugins()
+        {
+            $this->pro_plugins = array(
+                array(
+                    'name' => 'Essential Addons for Elementor - Pro',
+                    'url' => 'https://wpdeveloper.net/flexia-essential-addons-elementor',
+                    'desc' => 'Ultimate elements  library for Elementor WordPress Page Builder. 30+ stunning elements for Elementor.',
+                    'author' => 'Codetic',
+                    'author_url' => 'https://essential-addons.com',
+                    'banner' => get_template_directory_uri() . '/admin/img/eael-pro-banner.png',
+                    'doc_url' => 'https://essential-addons.com/elementor',
+                ),
+            );
+
+            foreach ($this->pro_plugins as $plugin) {
+                $this->render_premium_template(
+                    $plugin['name'],
+                    $plugin['url'],
+                    $plugin['desc'],
+                    $plugin['author'],
+                    $plugin['author_url'],
+                    $plugin['banner'],
+                    $plugin['doc_url']
+                );
+            }
+        }
+
+        /**
+         * This method will render recomended plugin block
+         *
+         * @since   1.0.0
+         */
+        public function render_free_template($plugin, $api, $slug_name, $file_name, $btn_class, $btn_text)
+        {
+            echo '<div class="flexia-plugins">
+				<div class="header">
+					<img src="' . $api->banners['high'] . '" alt="' . esc_attr($slug_name) . '">
+				</div>
+				<div class="content">
+					<h4 class="title">' . $api->name . '</h4>
+					<p>' . $api->short_description . '</p>
+					<span class="by-author">' . __('By:', 'flexia') . $api->author . '</span>
+				</div>
+				<div class="footer">
+					<div class="footer-left">
+						<ul>
+							<li>
+								<button type="submit" class="' . esc_attr($btn_class) . ' install-btn" data-slug = "' . esc_attr($slug_name) . '" data-file-name="' . esc_attr($file_name) . '">' . $btn_text . '</button>
+							</li>
+							<li><a href="https://wordpress.org/plugins/' . $api->slug . '/" target="_blank">' . __('More Details', 'flexia') . '</a></li>
+						</ul>
+					</div>
+					<div class="footer-right">
+						<span class="tag-free">' . __('Free', 'flexia') . '</span>
+					</div>
+				</div>
+			</div>';
+        }
+
+        /**
+         * This method will render premium plugin block
+         *
+         * @since   1.0.0
+         */
+        public function render_premium_template($name, $url, $desc, $author, $author_url, $banner, $doc_url)
+        {
+            echo '<div class="flexia-plugins">
+				<div class="header">
+					<img src="' . esc_url($banner) . '" alt="' . esc_attr($name) . '">
+				</div>
+				<div class="content">
+					<h4 class="title">' . __($name, 'felxia-core') . '</h4>
+					<p>' . esc_html($desc, 'flexia') . '</p>
+					<span class="by-author">' . __('By:', 'flexia') . ' <a href="' . esc_url($author_url) . '">' . esc_html($author, 'flexia') . '</a></span>
+				</div>
+				<div class="footer">
+					<div class="footer-left">
+						<ul>
+							<li>
+								<a href="' . esc_url($url) . '" class="button button-secondary">' . __('Get Now', 'flexia') . '</a>
+							</li>
+							<li><a href="' . esc_url($doc_url) . '" target="_blank">' . __('More Details', 'flexia') . '</a></li>
+						</ul>
+					</div>
+					<div class="footer-right">
+						<span class="tag-pro">' . __('Premium', 'flexia') . '</span>
+					</div>
+				</div>
+			</div>';
+        }
+
+    }
+}
--- functions.php
+++ functions.php
@@ -1,7 +1,9 @@
 <?php
 
 // No direct access, please
-if ( ! defined( 'ABSPATH' ) ) exit;
+if (!defined('ABSPATH')) {
+    exit;
+}
 
 /**
  * Some Definition
@@ -16,119 +18,120 @@ define('FLEXIA_DEV_MODE', true);
  * @package Flexia
  */
 
-if ( ! function_exists( 'flexia_setup' ) ) :
-/**
- * Sets up theme defaults and registers support for various WordPress features.
- *
- * Note that this function is hooked into the after_setup_theme hook, which
- * runs before the init hook. The init hook is too late for some features, such
- * as indicating support for post thumbnails.
- */
-function flexia_setup() {
-	/*
-	 * Make theme available for translation.
-	 * Translations can be filed in the /languages/ directory.
-	 * If you're building a theme based on Flexia, use a find and replace
-	 * to change 'flexia' to the name of your theme in all the template files.
-	 */
-	load_theme_textdomain( 'flexia', get_template_directory() . '/languages' );
+if (!function_exists('flexia_setup')) {
+    /**
+     * Sets up theme defaults and registers support for various WordPress features.
+     *
+     * Note that this function is hooked into the after_setup_theme hook, which
+     * runs before the init hook. The init hook is too late for some features, such
+     * as indicating support for post thumbnails.
+     */
+    function flexia_setup()
+    {
+        /*
+         * Make theme available for translation.
+         * Translations can be filed in the /languages/ directory.
+         * If you're building a theme based on Flexia, use a find and replace
+         * to change 'flexia' to the name of your theme in all the template files.
+         */
+        load_theme_textdomain('flexia', get_template_directory() . '/languages');
 
-	// Add default posts and comments RSS feed links to head.
-	add_theme_support( 'automatic-feed-links' );
+        // Add default posts and comments RSS feed links to head.
+        add_theme_support('automatic-feed-links');
 
-	/*
-	 * Let WordPress manage the document title.
-	 * By adding theme support, we declare that this theme does not use a
-	 * hard-coded <title> tag in the document head, and expect WordPress to
-	 * provide it for us.
-	 */
-	add_theme_support( 'title-tag' );
+        /*
+         * Let WordPress manage the document title.
+         * By adding theme support, we declare that this theme does not use a
+         * hard-coded <title> tag in the document head, and expect WordPress to
+         * provide it for us.
+         */
+        add_theme_support('title-tag');
 
-	/*
-	 * Enable support for Post Thumbnails on posts and pages.
-	 *
-	 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-	 */
-	add_theme_support( 'post-thumbnails' );
+        /*
+         * Enable support for Post Thumbnails on posts and pages.
+         *
+         * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+         */
+        add_theme_support('post-thumbnails');
 
-	add_image_size( 'flexia-featured-image', 2000, 1200, true );
+        add_image_size('flexia-featured-image', 2000, 1200, true);
 
-	add_image_size( 'flexia-thumbnail-avatar', 100, 100, true );
-	/*
-	 * This theme styles the visual editor to resemble the theme style
-	 */
-	add_editor_style( get_template_directory() . '/framework/assets/admin/css/editor-style.css' );
+        add_image_size('flexia-thumbnail-avatar', 100, 100, true);
+        /*
+         * This theme styles the visual editor to resemble the theme style
+         */
+        add_editor_style(get_template_directory() . '/framework/assets/admin/css/editor-style.css');
 
-	/**
-	 * Register primary menu
-	 */
-	register_nav_menus( array(
-		'primary' => esc_html__( 'Primary Menu', 'flexia' ),
-		'topbar-menu' => esc_html__( 'Topbar Menu', 'flexia' ),
-		'footer-menu' => esc_html__( 'Footer Menu', 'flexia' ),
-	) );
+        /**
+         * Register primary menu
+         */
+        register_nav_menus(array(
+            'primary' => esc_html__('Primary Menu', 'flexia'),
+            'topbar-menu' => esc_html__('Topbar Menu', 'flexia'),
+            'footer-menu' => esc_html__('Footer Menu', 'flexia'),
+        ));
 
+        /**
+         * Add search box to primary menu
+         */
 
-	/**
-	 * Add search box to primary menu
-	 */
+        function flexia_nav_search($items, $args)
+        {
+            $flexia_nav_menu_search = get_theme_mod('flexia_nav_menu_search', true);
 
-function flexia_nav_search($items, $args)
-{
-    $flexia_nav_menu_search = get_theme_mod('flexia_nav_menu_search', true);
+            if ($flexia_nav_menu_search == true && 'primary' === $args->theme_location) {
+                $items .= '<li class="menu-item navbar-search-menu"> <a id="btn-search" href="javascript:void(0);">';
+                $items .= '<i class="fa fa-search" aria-hidden="true"></i></a></li>';
+            }
+            return $items;
+        }
+        add_filter('wp_nav_menu_items', 'flexia_nav_search', 98, 2);
 
-		if ($flexia_nav_menu_search == true && 'primary' === $args->theme_location) {
-        $items .= '<li class="menu-item navbar-search-menu"> <a id="btn-search" href="javascript:void(0);">';
-        $items .= '<i class="fa fa-search" aria-hidden="true"></i></a></li>';
+        /**
+         * Enable support for Post Formats
+         */
+        add_theme_support('post-formats', array('aside', 'image', 'video', 'quote', 'link', 'status', 'chat'));
+
+        /**
+         * Enable support for WooCommerce
+         */
+        add_theme_support('woocommerce');
+
+        /*
+         * Switch default core markup for search form, comment form, and comments
+         * to output valid HTML5.
+         */
+        add_theme_support('html5', array(
+            'comment-form',
+            'comment-list',
+            'gallery',
+            'caption',
+        ));
+
+        // Set up the WordPress core custom background feature.
+        add_theme_support('custom-background', apply_filters('flexia_custom_background_args', array(
+            'default-color' => 'f9f9f9',
+            'default-image' => '',
+        )));
+
+        // Add theme support for selective refresh for widgets.
+        add_theme_support('customize-selective-refresh-widgets');
+
+        /**
+         * Add support for core custom logo.
+         *
+         * @link https://codex.wordpress.org/Theme_Logo
+         */
+        add_theme_support('custom-logo', array(
+            'height' => 80,
+            'width' => 250,
+            'flex-width' => true,
+            'flex-height' => true,
+        ));
+
     }
-    return $items;
 }
-add_filter('wp_nav_menu_items', 'flexia_nav_search', 98, 2);
-
-	/**
-	 * Enable support for Post Formats
-	 */
-	add_theme_support( 'post-formats', array( 'aside', 'image', 'video', 'quote', 'link', 'status', 'chat' ) );
-
-	/**
-	 * Enable support for WooCommerce
-	 */
-	add_theme_support('woocommerce');
-	/*
-	 * Switch default core markup for search form, comment form, and comments
-	 * to output valid HTML5.
-	 */
-	add_theme_support( 'html5', array(
-		'comment-form',
-		'comment-list',
-		'gallery',
-		'caption',
-	) );
-
-	// Set up the WordPress core custom background feature.
-	add_theme_support( 'custom-background', apply_filters( 'flexia_custom_background_args', array(
-		'default-color' => 'f9f9f9',
-		'default-image' => '',
-	) ) );
-
-	// Add theme support for selective refresh for widgets.
-	add_theme_support( 'customize-selective-refresh-widgets' );
-
-	/**
-	 * Add support for core custom logo.
-	 *
-	 * @link https://codex.wordpress.org/Theme_Logo
-	 */
-	add_theme_support( 'custom-logo', array(
-	   'height'      => 80,
-	   'width'       => 250,
-	   'flex-width' => true,
-	   'flex-height' => true,
-	) );
-
-}
-endif;
-add_action( 'after_setup_theme', 'flexia_setup' );
+add_action('after_setup_theme', 'flexia_setup');
 
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
@@ -137,11 +140,11 @@ add_action( 'after_setup_theme', 'flexia_setup' );
  *
  * @global int $content_width
  */
-function flexia_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'flexia_content_width', 640 );
+function flexia_content_width()
+{
+    $GLOBALS['content_width'] = apply_filters('flexia_content_width', 640);
 }
-add_action( 'after_setup_theme', 'flexia_content_width', 0 );
-
+add_action('after_setup_theme', 'flexia_content_width', 0);
 
 /**
  * Custom template tags for this theme.
@@ -184,15 +187,14 @@ require get_template_directory() . '/framework/functions/flexia/breadcrumb.php';
  * Integrations.
  */
 
-if( class_exists('WooCommerce') )  {
-	require get_template_directory() . '/framework/functions/flexia/integrations/woocommerce/woocommerce-integration.php';
-	require get_template_directory() . '/framework/functions/flexia/integrations/woocommerce/class-flexia-woocommerce.php';
+if (class_exists('WooCommerce')) {
+    require get_template_directory() . '/framework/functions/flexia/integrations/woocommerce/woocommerce-integration.php';
+    require get_template_directory() . '/framework/functions/flexia/integrations/woocommerce/class-flexia-woocommerce.php';
 }
 
 if (class_exists('Easy_Digital_Downloads')) {
     require get_template_directory() . '/framework/functions/flexia/integrations/edd/edd-integration.php';
 }
-
 
 /**
  * Customizer additions.
@@ -201,91 +203,82 @@ if (class_exists('Easy_Digital_Downloads')) {
 require get_template_directory() . '/framework/functions/customizer/defaults.php';
 require get_template_directory() . '/framework/functions/customizer/customizer.php';
 
-
-// Admin only classes
-if ( is_admin() ) {
-
-	// Recommend plugins
-	require_once( get_template_directory() .'/inc/plugins/class-tgm-plugin-activation.php' );
-	require_once( get_template_directory() .'/inc/plugins/tgm-plugin-activation.php' );
-
+// Admin functionalities
+if (is_admin()) {
+    require_once get_template_directory() . '/admin/admin.php';
+    
+    new Flexia_Admin();
 }
 
 /**
  * Load Jetpack compatibility file.
  */
-if ( defined( 'JETPACK__VERSION' ) ) {
-	require get_template_directory() . '/inc/jetpack.php';
+if (defined('JETPACK__VERSION')) {
+    require get_template_directory() . '/inc/jetpack.php';
 }
-
 
 /********* Google Fonts URL function  ***********/
 
-if ( ! function_exists( 'flexia_fonts_url' ) ){
-	function flexia_fonts_url() {
-	    $fonts_url = '';
-	    $content_font = get_theme_mod('body_font_family', '');
-	    $content_font_variant = get_theme_mod( 'body_font_variants', '' );
-	    $content_font_subset = get_theme_mod( 'body_font_subsets', '' );
+if (!function_exists('flexia_fonts_url')) {
+    function flexia_fonts_url()
+    {
+        $fonts_url = '';
+        $content_font = get_theme_mod('body_font_family', '');
+        $content_font_variant = get_theme_mod('body_font_variants', '');
+        $content_font_subset = get_theme_mod('body_font_subsets', '');
 
-	    $header_font = get_theme_mod('heading_font_family', '');
-	    $header_font_variant = get_theme_mod( 'heading_font_variants', '' );
-	    $header_font_subset = get_theme_mod( 'heading_font_subsets', '' );
+        $header_font = get_theme_mod('heading_font_family', '');
+        $header_font_variant = get_theme_mod('heading_font_variants', '');
+        $header_font_subset = get_theme_mod('heading_font_subsets', '');
 
-	    if ( 'off' !== $content_font || 'off' !== $header_font ) {
-	        $font_families = array();
+        if ('off' !== $content_font || 'off' !== $header_font) {
+            $font_families = array();
 
-	        if ( 'off' !== $content_font ) {
-	            $font_families[] = $content_font . ':'.$content_font_variant.'&amp;'.$content_font_subset;
-	        }
+            if ('off' !== $content_font) {
+                $font_families[] = $content_font . ':' . $content_font_variant . '&amp;' . $content_font_subset;
+            }
 
-	        if ( 'off' !== $header_font ) {
-	            $font_families[] = $header_font . ':'.$header_font_variant.'&amp;'.$header_font_subset;
-	        }
+            if ('off' !== $header_font) {
+                $font_families[] = $header_font . ':' . $header_font_variant . '&amp;' . $header_font_subset;
+            }
 
-	        $query_args = array(
-	            'family' => urlencode( implode( '|', array_unique($font_families) ) ),
-	        );
+            $query_args = array(
+                'family' => urlencode(implode('|', array_unique($font_families))),
+            );
 
-	        $fonts_url = add_query_arg( $query_args, 'https://fonts.googleapis.com/css' );
-	    }
+            $fonts_url = add_query_arg($query_args, 'https://fonts.googleapis.com/css');
+        }
 
-	    return esc_url_raw( $fonts_url );
-	}
-}
-
-// Admin notice for Flexia Core
-
-if( class_exists( 'Flexia_Core' ) ) {
-  return false;
-} else {
-	add_action('admin_notices', 'flexia_admin_notice');
-}
-
-function flexia_admin_notice() {
-if ( current_user_can( 'install_plugins' ) )
-   {
-  global $current_user ;
-        $user_id = $current_user->ID;
-        /* Check that the user hasn't already clicked to ignore the message */
-
-        $flexia_admin_notice_text = __( 'Thanks for using <strong>Flexia!</strong> We have added lots of cool features through <strong>Flexia Core</strong> and we recommend to keep the plugin active for maximum features.<a href="admin.php?page=flexia-required-plugins" style="text-decoration: none;"><span class="dashicons dashicons-admin-plugins" style="margin-top: -1px;margin-left: 10px;"></span> Install Plugin</a><a href="%1$s" style="text-decoration: none; margin-left: 10px;"><span class="dashicons dashicons-dismiss"></span> Dismiss</a>', 'flexia' );
-
-  if ( ! get_user_meta($user_id, 'flexia_ignore_notice006') ) {
-        echo '<div class="flexia-admin-notice updated"><p>';
-        printf($flexia_admin_notice_text,  admin_url( 'admin.php?page=flexia-required-plugins&flexia_nag_ignore=0' ));
-        echo "</p></div>";
-  }
+        return esc_url_raw($fonts_url);
     }
 }
 
+// Mute admin notice
+function flexia_nag_ignore()
+{
+    if (isset($_GET['flexia_nag_ignore']) && $_GET['flexia_nag_ignore'] == '0') {
+        update_user_meta(get_current_user_id(), 'flexia_ignore_notice006', true);
+    }
+}
 add_action('admin_init', 'flexia_nag_ignore');
 
-function flexia_nag_ignore() {
-  global $current_user;
-        $user_id = $current_user->ID;
-        /* If user clicks to ignore the notice, add that to their user meta */
-        if ( isset($_GET['flexia_nag_ignore']) && '0' == $_GET['flexia_nag_ignore'] ) {
-             add_user_meta($user_id, 'flexia_ignore_notice006', 'true', true);
-  }
+// Flexia admin notices
+function flexia_admin_notices()
+{
+    if (!class_exists('Flexia_Core')) {
+        if (current_user_can('install_plugins')) {
+            $flexia_admin_notice_text = __('Thanks for using <strong>Flexia!</strong> We have added lots of cool features through <strong>Flexia Core</strong> and we recommend to keep the plugin active for maximum features.<a href="%1$s" style="text-decoration: none;"><span class="dashicons dashicons-admin-plugins" style="margin-top: -1px;margin-left: 10px;"></span> Install Plugin</a><a href="%2$s" style="text-decoration: none; margin-left: 10px;"><span class="dashicons dashicons-dismiss"></span> Dismiss</a>', 'flexia');
+
+            if (!get_user_meta(get_current_user_id(), 'flexia_ignore_notice006', true)) {
+                echo '<div class="flexia-admin-notice updated"><p>'
+                . sprintf(
+                    $flexia_admin_notice_text,
+                    admin_url('admin.php?page=flexia-recommended-plugins'),
+                    admin_url('admin.php?page=flexia-recommended-plugins&flexia_nag_ignore=0')
+                ) .
+                '</p></div>';
+            }
+        }
+    }
 }
+add_action('admin_notices', 'flexia_admin_notices');
