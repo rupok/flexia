db80a750026d00085d0a48e0c7f052f9099ba055
Rupok <re.enter.rupok@gmail.com>
2020-05-12 15:11:39 +0600

    Gulp files added

---
+++ framework/assets/gulpfile.js
@@ -0,0 +1,67 @@
+const gulp = require('gulp');
+const compass = require('gulp-compass');
+const notify = require('gulp-notify');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+
+/**
+ * Primary Compass Configurations
+ * Note that all settings besides css_dir and sass_dir can just go in config.rb.
+ */
+var compassConfig = {
+	css: 'sass/',     // Must match css_dir value in config.rb.
+	sass: 'sass/',     	  // Must match sass_dir value in config.rb
+	config_file: 'config.rb',
+	sourcemap: true
+};
+
+/**
+ * Files and directories we reference in our tasks below. The "toCompile" properties
+ * also generally contain patterns of files that are watched to trigger compilation.
+ */
+var files = {
+	sass: {
+		toCompile: ['sass/*.scss', 'sass/*/*.scss']
+	}
+};
+
+// -------------
+// Define Tasks.
+// -------------
+
+// Compile SASS
+
+gulp.task('compassBuild', function() {
+	return gulp.src(files.sass.toCompile)
+		.pipe(
+			compass(compassConfig)
+			.on('error', notify.onError({
+				message: 'Sass/Compass failed. Check console for errors.'
+			}))
+			.on('error', function(error) {
+				console.log(error); // find the error about
+				this.emit('end');
+			})
+		)
+		.pipe(autoprefixer({
+			browsers: ['last 10 versions']
+		}))
+		.pipe(gulp.dest('./' + compassConfig.css))
+		.pipe(notify('Sass/Compass successfully compiled'));
+});
+
+// Minify CSS
+
+gulp.task('minify-css', () => {
+  return gulp.src('sass/style.css')
+	.pipe( cleanCSS() ) // minify the css 
+    .pipe(gulp.dest('site/css/'));
+});
+
+
+// Default task (one-time build).
+gulp.task('default', ['compassBuild', 'minify-css']);
+
+
+// Gulp Watcher if there is any change on SCSS file.
+gulp.watch(files.sass.toCompile, ['default']);
