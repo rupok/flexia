1f127e2f235a1b7ab9f8ff5e616a25636377a15c
fencer monir <fencemonir@gmail.com>
2021-02-24 18:09:22 +0600

    Update Header 4 Css

--- framework/views/template-parts/headers/header-4.php
+++ framework/views/template-parts/headers/header-4.php
@@ -22,7 +22,27 @@ $dropdown_animation = 'flexia-menu-dropdown-animate-' . get_theme_mod('flexia_ma
 <div class="flexia-navbar <?php echo esc_attr($navbar_class); ?>">
     <div class="flexia-navbar-container">
         <div class="flexia-container flexia-navbar-inner <?php echo (get_theme_mod('flexia_header_layout_type') == "full-width") ? "full-width" : "width max" ?>">
+            
+                <!-- Header Main Social Start -->
+                <div class="flexia-header-4-mobile">
+                    <?php
+                    if (flexia_get_option('flexia_enable_header_social') == true && flexia_get_option('flexia_header_social_position') == "header_main") {
+                        get_template_part('framework/views/template-parts/content', 'social-links');
+                    }
+                    ?>
+                </div>
+                <!-- Header Main Social End -->
             <div class="flexia-header-top">
+                <div class="flexia-logobar-inline">
+                    <div class="site-branding">
+
+                        <?php echo flexia_main_logo(); ?>
+
+                    </div><!-- .site-branding -->
+                </div><!-- flexia-logobar-inline -->
+            </div>
+            <div class="flexia-header-navigation">
+
                 <!-- Header Main Social Start -->
                 <div class="flexia-header-4-social">
                     <?php
@@ -32,25 +52,6 @@ $dropdown_animation = 'flexia-menu-dropdown-animate-' . get_theme_mod('flexia_ma
                     ?>
                 </div>
                 <!-- Header Main Social End -->
-
-                <div class="flexia-logobar-inline">
-                    <div class="site-branding">
-
-                        <?php echo flexia_main_logo(); ?>
-
-                    </div><!-- .site-branding -->
-                </div><!-- flexia-logobar-inline -->
-                <div class="flexia-menu header-icons icon-menu">
-                    <ul class="nav-menu <?php echo esc_attr($dropdown_animation); ?>">
-                        <?php
-                        $items = '';
-                        $items = apply_filters('flexia_header_icon_items', $items);
-                        echo wp_kses_post($items);
-                        ?>
-                    </ul>
-                </div><!-- #header-icons -->
-            </div>
-            <div class="flexia-header-navigation">
                 <nav id="site-navigation" class="flexia-menu main-navigation">
                     <?php
 
@@ -68,6 +69,16 @@ $dropdown_animation = 'flexia-menu-dropdown-animate-' . get_theme_mod('flexia_ma
                     ?>
 
                 </nav><!-- #site-navigation -->
+                <div class="flexia-menu header-icons icon-menu">
+                    <ul class="nav-menu <?php echo esc_attr($dropdown_animation); ?>">
+                        <?php
+                        $items = '';
+                        $items = apply_filters('flexia_header_icon_items', $items);
+                        echo wp_kses_post($items);
+                        ?>
+                    </ul>
+                </div><!-- #header-icons -->
+
             </div><!-- #flexia-header-navigation -->
         </div><!-- #flexia-container -->
     </div><!-- #flexia-navbar-container -->
