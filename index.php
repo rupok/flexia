ff5ff90a3a8124673768346eb15ab05ef6c0a863
Jamil Uddin <jamilbd07@gmail.com>
2023-10-17 19:02:36 +0600

    implementing block base theme

--- index.php
+++ index.php
@@ -13,66 +13,6 @@
  * @package Flexia
  */
 
-get_header();
-
-?>
-
-
-<?php
-/**
- * Flexia hook for Blog Header
- *
- * @since   v2.1.3
- */
-?>
-
-
-<div id="content" class="site-content">
-
-	<?php do_action('flexia_blog_header'); ?>
-
-	<div class="flexia-wrapper flexia-container max width">
-
-		<div id="primary" class="content-area">
-
-			<?php
-			/**
-			 * Flexia hook for Blog Before Main Content/ Loop
-			 *
-			 * @since   v2.1.3
-			 */
-
-			do_action('flexia_blog_before_content');
-			?>
-
-			<?php
-			/**
-			 * Flexia hook for Blog Content/The Loop
-			 *
-			 * @since   v2.1.3
-			 */
-
-			do_action('flexia_blog_content');
-			?>
-
-
-			<?php
-			/**
-			 * Flexia hook for Blog After Main Content/ Loop
-			 *
-			 * @since   v2.1.3
-			 */
-
-			do_action('flexia_blog_after_content');
-			?>
-
-		</div><!-- #primary -->
-
-		<?php get_sidebar(); ?>
-
-	</div><!-- #flexia-wrapper -->
-</div><!-- #content -->
-</div><!-- #page -->
-
-<?php
-get_footer();
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly.
+}
